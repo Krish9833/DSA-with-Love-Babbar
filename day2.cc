779c84b350baf93baefee09f21bf5641368de0bf
Krish9833 <129373586+Krish9833@users.noreply.github.com>
2023-12-18 17:26:22 +0000

    if else statement

---
+++ day2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main () {
+    
+ int age;
+ //print enter the score
+
+  cout << "enter the age" << endl;
+ // take input into score variable
+  cin >> age;
+ //if condition
+  if (age>=18) {
+    cout << " I can vote " << endl ;
+  }
+  else{
+    cout << " I cannot vote " << endl ;
+  }
+}
+// cout << "mere bhagwaan mata pitaji\n"  << endl ;
+//cout << "\n";
---
+++ main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+  int a =5;
+
+ cout << "value of a is " << a << endl ;
+ // char ch ='a';
+ // cout <<"value of ch is " << ch << endl ;
+ // bool flag = 1 ;
+  //cout << "value of flag is " << flag >> endl  ;
+}
+// cout << "mere bhagwaan mata pitaji\n"  << endl ;
+//cout << "\n";
