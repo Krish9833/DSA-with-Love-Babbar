1897c03fd5349e4f6142f3e0b937bda44aa87393
Krish9833 <129373586+Krish9833@users.noreply.github.com>
2023-12-19 09:19:46 +0000

    patterns

---
+++ day2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main () {
+    
+ //int age;
+ //print enter the score
+
+  //cout << "enter the age" << endl;
+ // take input into score variable
+  //cin >> age;
+ //if condition
+  //if (age>=18) {
+    //cout << " I can vote " << endl ;
+  //}
+  //else{
+    //cout << " I cannot vote " << endl ;
+  //}
+ //for (int i=1; i<10; i=i*2) {
+//cout << i  << endl;
+ //}
+ //for (int i=100; i>0 ;i=i/2){
+ // cout << i << endl;
+ //}
+ //for(int i=1; i<=5; i=i+2){
+  //cout << i << endl;
+ //}
+ for (int row=1; row<4; row = row+1)
+ {
+  for (int col=1; col<6; col= col+1)
+     {cout << "*";
+     }
+     cout << endl;
+ }
+}
