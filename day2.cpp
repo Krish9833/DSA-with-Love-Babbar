47d554e6cf9bd367259ad1d7bc01cb576c48c78d
Krish9833 <129373586+Krish9833@users.noreply.github.com>
2023-12-19 09:35:14 +0000

    solid rectangle patterns

--- day2.cpp
+++ day2.cpp
@@ -24,11 +24,26 @@ int main () {
  //for(int i=1; i<=5; i=i+2){
   //cout << i << endl;
  //}
- for (int row=1; row<4; row = row+1)
- {
-  for (int col=1; col<6; col= col+1)
-     {cout << "*";
-     }
-     cout << endl;
- }
+ // print solid rectangle
+ //for (int row=1; row<4; row = row+1)
+ //{
+  //for (int col=1; col<6; col= col+1)
+     //{cout << "*";
+    // }
+  //   cout << endl;
+//}
+//row +=1 is same as row=row+1
+//print solid rctangle for 4 row
+for ( int row = 0; row <6; row+=1)
+//inner loop
+{
+  for (int col=0;col<6; col+=1) 
+  {
+    cout << "*";
+}
+cout << endl;
+
+  
+}
+
 }
